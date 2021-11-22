b3801c68026377d240024cae4b33b2b150504e5d
Aditya <abhogte@gmail.com>
2021-11-22 16:23:06 -0600

    complete sprint pre-course1-plesereview

---
+++ Exercise_2.py
@@ -0,0 +1,45 @@
+# Time Complexit - O(n)
+# Space Complexity - O(1)
+
+
+class Node:
+    def __init__(self, data):
+       self.data = data
+       self.next = None
+ 
+class Stack:
+    def __init__(self):
+        self.head = None
+        
+
+        
+    def push(self, data):
+        self.head = self.Node(data, self.head)
+         
+        
+    def pop(self):
+        if self.head == None:
+            print("Stack is empty")
+        popelement = self.head.data
+        self.head = self.head.next
+
+        
+a_stack = Stack()
+while True:
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print('push <value>')
+    print('pop')
+    print('quit')
+    do = input('What would you like to do? ').split()
+    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    operation = do[0].strip().lower()
+    if operation == 'push':
+        a_stack.push(int(do[1]))
+    elif operation == 'pop':
+        popped = a_stack.pop()
+        if popped is None:
+            print('Stack is empty.')
+        else:
+            print('Popped value: ', int(popped))
+    elif operation == 'quit':
+        break
